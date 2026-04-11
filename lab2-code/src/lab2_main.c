e44b6a65b98b7ad400a96bf9d31d75fa2edff40c
sharlitwong <charlotte.wong@tufts.edu>
2026-04-10 20:35:57 -0400

    communication with pi works. sending 2 bytes per motor.

--- lab2-code/src/lab2_main.c
+++ lab2-code/src/lab2_main.c
@@ -6,15 +6,9 @@
 #include "stm32l4xx_hal.h"
 #include "interupts_file.h"
 #include "motor_control_file.h"
-
-int speed=0;
-char command=0; 
-//new stuff from pi..??
-uint16_t tof_distance = 0;  // ADD THIS
-bool close = false;          // ADD THIS
+#include <stdlib.h>
 
 volatile uint32_t system_ticks = 0;
-//UART_HandleTypeDef huart1; apparently unecessary?
 
 // config the system time 
 void SysTick_Init(uint32_t tick_hz)
@@ -34,91 +28,56 @@ uint64_t cur_time_ms(void){
     return system_ticks;
 }
 
-// default motor control function to handle the motor controls.
-// TODO: FIGURE OUT HOW TO DRIVE THE MOTORS
-// IMPLEMENT PID CONTROL FOR TURNING x amount of Degrees with the bot so slows down to rotate correctly, 
-//(in case need to rotate the bot a persise amount)
-//Needs to also be able to drive forward, backward, sideways and capible of rotating a certain amount of degrees.
-// should be able to do this with the econders themselves. 
-// extern volatile int previous_state_enc1;
-// extern volatile int enc_ticks1;
-// // emcpder 2
-// extern volatile int previous_state_enc2;
-// extern volatile int enc_ticks2;
-// // emcpder 3
-// extern volatile int previous_state_enc3;
-// extern volatile int enc_ticks3;
-// // encoder 4
-// extern volatile int previous_state_enc4;
-// extern volatile int enc_ticks4;
-int control_motors(char command, int speed, int ticks){
+/*
+ * control_motors
+ * the MSB (sign) of each parameter indicates the direction in which a wheel
+ * will turn in
+ * - 0 = clockwise (positive numbers), 1 = counterclockwise (negative numbers)
+ * - clockwise versus counterclockwise is determined by looking at the wheel 
+ * face on from from outside the bot's circular perimeter
+ * the magnitude of each parameter indicates the speed at which a wheel will 
+ * turn in
+ */
+int control_motors(int16_t front, int16_t right, int16_t back, int16_t left) {
     char buffer[100]; // Declare a character array to store the formatted string
-    //sprintf(buffer,"current command was %c \n", command);
-    //serial_write(USART2, buffer);
-    if(command == 'f'){ //forward
-        //enable speed
-        change_duty(1, 0); //top
-        change_duty(2, speed); //left
-        change_duty(3, 0); //bottom
-        change_duty(4, speed); //right
+    //positive = clockwise
+    bool dir[4] = {}; //1 = counterclockwise, 0 = clockwise
+    dir[0] = (front < 0); //will be 1 if negative, meaning counterclockwise
+    dir[1] = (right < 0);
+    dir[2] = (back < 0);
+    dir[3] = (left < 0);
 
-    //phase directionq1 //bottom
-        change_duty(4, speed); //right
-        //directions
-        set_pin(GPIOB, 0, 1); //J1 left backward
-        set_pin(GPIOB, 1, 0); //J2 bottom not moving
-        set_pin(GPIOB, 4, 0); //J3 top not moving
-        set_pin(GPIOB, 5, 0); //J4 right backward
-    }else if(command == 'l'){ //left
-        //speed
-        change_duty(1, speed); //top
-        change_duty(2, 0); //left
-        change_duty(3, speed); //bottom
-        change_duty(4, 0); //right
-        //direction
-        set_pin(GPIOB, 0, 1); //J1 left not moving
-        set_pin(GPIOB, 1, 1); //J2 bottom forward
-        set_pin(GPIOB, 4, 1); //J3 top forward
-        set_pin(GPIOB, 5, 1); //J4 right not moving
-    }else if(command == 'r'){ //right
-        //speed
-        change_duty(1, speed); //top
-        change_duty(2, 0); //left
-        change_duty(3, speed); //bottom
-        change_duty(4, 0); //right
-        //direction
-        set_pin(GPIOB, 0, 0); // J1 left not moving
-        set_pin(GPIOB, 1, 0); //J2 bottom backward
-        set_pin(GPIOB, 4, 0); //J3 top backward
-       set_pin(GPIOB, 5, 0); //J4 right not moving
+    int spd[4] = {}; //our change_duty function can only take in values 0-1000
+    spd[0] = (abs(front) * 1000) / 32767;
+    spd[1] = (abs(right) * 1000) / 32767;
+    spd[2] = (abs(back) * 1000) / 32767;
+    spd[3] = (abs(left) * 1000) / 32767;
 
-    }else if(command == 'b'){ //back 
-        //speed
-        change_duty(1, 0); //top
-        change_duty(2, speed); //left
-        change_duty(3, 0); //bottom
-        change_duty(4, speed); //right
-        //direction
-        set_pin(GPIOB, 0, 0); // J1 left not moving
-        set_pin(GPIOB, 1, 0); //J2 bottom backward
-        set_pin(GPIOB, 4, 0); //J3 top backward
-        set_pin(GPIOB, 5, 1); //J4 right not moving
-    }
-    else if (command == 's'){
-        change_duty(1, 0); //top
-        change_duty(2, 0); //left
-        change_duty(3, 0); //bottom
-        change_duty(4, 0); //right
-        //direction
-        set_pin(GPIOB, 0, 1); // J1 left not moving
-        set_pin(GPIOB, 1, 0); //J2 bottom backward
-        set_pin(GPIOB, 4, 0); //J3 top backward
-        set_pin(GPIOB, 5, 1); //J4 right not moving
-    }
+    //FRONT motor direction
+    set_pin(GPIOB, 4, dir[0]); //J3 top backward, 4 = top/front motor, 
+    //0 = counterclockwise for this motor
+    //front motor speed
+    change_duty(1, spd[0]); //top
+
+    //RIGHT motor direction
+    set_pin(GPIOB, 5, !dir[1]); //J4 right
+    //0 = clockwise for this motor
+    //right motor speed
+    change_duty(4, spd[1] + 1000); //right
+
+    //BACK motor direction
+    set_pin(GPIOB, 1, !dir[2]); //J2 bottom
+    //0 = clockwise for this motor
+    //back motor speed
+    change_duty(3, spd[2]); //bottom
+
+    //LEFT motor direction
+    set_pin(GPIOB, 0, dir[3]); //J1 left 
+    //left motor speed
+    change_duty(2, spd[3] + 1000); //left
 }
 
-int main()
-{
+int main() {
     //set the clock to 80 mhz
     clock_setup_80MHz(); 
     //set the uart to start
@@ -140,80 +99,27 @@ int main()
     // set up prompt
     char prompt[] = "TEST \n\0";
     
-
     for(;;){
-
         // Temporary test - remove after debugging
         serial_write(USART2, "Waiting for USART1 byte...\n");
         while (!(USART1->ISR & USART_ISR_RXNE));  // spin until a byte arrives
-        // char test = USART1->RDR;
-        // char msg[50];
-        // sprintf(msg, "Got byte: 0x%02X\n", test);
-        // serial_write(USART2, msg);
 
-        // speed=RXBUFFER2[1];
-        //command=RXBUFFER2[0]; 
-        //command = RXBUFFER[0];
-        char direction = (RXBUFFER[0]); //first byte is character command
-        //int speed = (RXBUFFER[0] );
-        uint16_t spd = (RXBUFFER[1] << 8) | RXBUFFER[2]; //2nd and 3rd byte are speed
-        int speed = (int)spd;
-        // serial_write(USART2, prompt);
-        //serial_read(USART2);
+        int16_t spd_f = (RXBUFFER[0] << 8) | RXBUFFER[1]; //front
+        int16_t spd_r = (RXBUFFER[2] << 8) | RXBUFFER[3]; //right
+        int16_t spd_b = (RXBUFFER[4] << 8) | RXBUFFER[5]; //back
+        int16_t spd_l = (RXBUFFER[6] << 8) | RXBUFFER[7]; //left
 
-        control_motors(direction, speed, 0);
-
-        // Data is already in RXBUFFER from the interrupt!
-        //tof_distance = (RXBUFFER[0] << 8) | RXBUFFER[1];  // Read MSB and LSB
-        // uint16_t tof_front = (RXBUFFER[0] << 8) | RXBUFFER[1];
-        // uint16_t tof_back  = (RXBUFFER[2] << 8) | RXBUFFER[3];
-        // uint16_t tof_left  = (RXBUFFER[4] << 8) | RXBUFFER[5];
-        // uint16_t tof_right = (RXBUFFER[6] << 8) | RXBUFFER[7];
+        // control_motors(direction, speed, 0);
+        control_motors(spd_f, spd_r, spd_b, spd_l);
         
-        // Check if close
-        //close = (tof_distance < 50);
-        
-        //Control motors based on distance
-        // if (command == 'f' && tof_front < 50) {
-        //     control_motors('s', 0, 0);
-        // } else if (command == 'b' && tof_back < 50) {
-        //     control_motors('s', 0, 0);
-        // } else if (command == 'l' && tof_left < 50) {
-        //     control_motors('s', 0, 0);
-        // } else if (command == 'r' && tof_right < 50) {
-        //     control_motors('s', 0, 0);
-        // } else {
-        //     control_motors(command, 1000, 0);
-        // }
-    
-
-        // serial_write(USART2, RXBUFFER);
-        
-        // Debug: print all sensor readings and current command
+        // Debug: print all received motor commands from raspberry pi
         char debug[150];
-        // sprintf(debug, "CMD:%c F:%u B:%u L:%u R:%u\n", command, tof_front, tof_back, tof_left, tof_right);
-        //sprintf(debug, "F:%u B:%u L:%u R:%u\n", tof_front, tof_back, tof_left, tof_right);
-        sprintf(debug, "CMD:%c SPD:%d RAW:[%02X %02X %02X]\n", direction, speed, RXBUFFER[0], RXBUFFER[1], RXBUFFER[2]);
+        sprintf(debug, "front: %d right: %d back: %d left: %d", spd_f, spd_r, spd_b, spd_l);
         serial_write(USART2, debug);
 
-        // char debug[150];
-        // sprintf(debug, "RAW:%02X %02X %02X %02X %02X %02X %02X %02X\n", 
-        // // command, tof_front, tof_back, tof_left, tof_right,
-        // RXBUFFER[0], RXBUFFER[1], RXBUFFER[2], RXBUFFER[3],
-        // RXBUFFER[4], RXBUFFER[5], RXBUFFER[6], RXBUFFER[7]);
-        // serial_write(USART2, debug);
-        
-        // printf("Front: %d\n Back: %d\n Left", );
-
-        // serial_write(USART2,prompt);
         for(int i=0; i<100000;i++){
             int t=0;
-        // }
-        // memcpy(prompt,(int*)(TIM1->CNT),strlen(prompt));
-        // serial_write(USART2, prompt);
-        
         }
-
 }
 return 0;
 }
